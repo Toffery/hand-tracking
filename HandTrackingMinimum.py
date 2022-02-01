82129d4b6c173d5f593e99b2d63da49a10c92e05
toffery <loltoffery@gmail.com>
2022-02-01 23:37:14 +0300

    handtracking

---
+++ HandTrackingMinimum.py
@@ -0,0 +1,38 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+
+prev_time = 0
+cur_time = 0
+
+while True:
+    success, img = cap.read()
+
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img_rgb)
+    # print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for hand_lms in results.multi_hand_landmarks:  # hand_lms - hand landmarks
+            for id, land_mark in enumerate(hand_lms.landmark):
+                height, width, channel = img.shape
+                cx, cy = int(land_mark.x * width), int(land_mark.y * height)
+                if id == 4 or id == 8 or id == 12:
+                    cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+
+            mp_draw.draw_landmarks(img, hand_lms, mp_hands.HAND_CONNECTIONS)
+
+
+    cur_time = time.time()
+    fps = 1 / (cur_time - prev_time)
+    prev_time = cur_time
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+    cv2.imshow('Window', img)
+    cv2.waitKey(1)
